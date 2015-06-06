c7f34de245ec0dc66e17393e746d6f9715a56f06
Derek Schwechel <Derek.Schwechel@gmail.com>
2015-06-06 13:20:53 -0500

    Database set up

---
+++ Argo/Models/ArgoContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Argo.Models
+{
+    public class ArgoContext : DbContext
+    {
+        public DbSet<Trip> Trips { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Bus> Buses { get; set; }
+    }
+}
---
+++ Argo/Models/Trip.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Argo.Models
+{
+    public class Trip
+    {
+        public int Id { get; set; }
+        public string Departure { get; set; }
+        public string Destination { get; set; }
+        public DateTime DepartTime { get; set; }
+        public DateTime ReturnTime { get; set; }
+        public int Type { get; set; }
+        public string Event { get; set; }
+        public int Busid { get; set; }
+    }
+}
